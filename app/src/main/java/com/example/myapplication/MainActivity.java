d8d1bdad34cc65d82b8acadf77ddf343f1777a8e
faisal-t <faizalmahromi35@gmail.com>
2019-10-22 22:04:12 +0700

    maaf mang blom selesai

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,9 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         gridView = findViewById(R.id.grid_view);
 
         MainAdapter adapter = new MainAdapter(this, R.layout.row_item, teams);
         gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posisi, long l) {
+                Intent intent = new Intent(getApplicationContext(),ProsesVote.class);
+
+                intent.putExtra("posisi",posisi);
+                startActivity(intent);
+            }
+        });
+
     }
 }
